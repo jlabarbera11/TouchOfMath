4523413e2688582991b1058a2339a8b2a0bdb515
Zachary <zsclapper7@gmail.com>
2011-10-18 00:23:24 -0400

    Code formatting and more efficient 'displayTables.js' definitions

--- displayTables.js
+++ displayTables.js
@@ -1,46 +1,47 @@
 //	Whether or not the operator and its parameters require unique display settings
-var specialDisplayOperator = new Array();
-
-specialDisplayOperator["P"] = true;
-specialDisplayOperator["sum"] = true;
-specialDisplayOperator["cos"] = true;
-specialDisplayOperator["arccos"] = true;
-specialDisplayOperator["!"] = false;
-specialDisplayOperator["^"] = true;
-specialDisplayOperator["rt"] = true;
-specialDisplayOperator["*"] = false;
-specialDisplayOperator["/"] = true;
-specialDisplayOperator["+"] = false;
-specialDisplayOperator["-"] = false;
-specialDisplayOperator["="] = false;
-specialDisplayOperator["parentheses"] = true;
-
+var specialDisplayOperator =
+{
+    "P": true,
+    "sum": true,
+    "cos": true,
+    "arccos": true,
+    "!": false,
+    "^": true,
+    "rt": true,
+    "*": false,
+    "/": true,
+    "+": false,
+    "-": false,
+    "=": false,
+    "parentheses": true
+};
 
 //	What to display as the operator-literal
-var operatorCharacters = new Array();
-
-operatorCharacters["="] = "=";
-operatorCharacters["*"] = "&sdot;";
-operatorCharacters["/"] = "/";
-operatorCharacters["+"] = "+";
-operatorCharacters["-"] = "&minus;";
-operatorCharacters["sum"] = "&sum;";
-operatorCharacters["^"] = "^";
-
+var operatorCharacters =
+{
+    "=": "=",
+    "*": "&sdot;",
+    "/": "/",
+    "+": "+",
+    "-": "&minus;",
+    "sum": "&sum;",
+    "^": "^"
+};
 
 //	The string to use in a css classname coresponding to the operator
-var operatorClassNameCharacters = new Array();
-
-operatorClassNameCharacters["P"] = "P";
-operatorClassNameCharacters["sum"] = "Sum";
-operatorClassNameCharacters["cos"] = "Cos";
-operatorClassNameCharacters["arccos"] = "Arccos";
-operatorClassNameCharacters["!"] = "Factorial";
-operatorClassNameCharacters["^"] = "Power";
-operatorClassNameCharacters["rt"] = "Root";
-operatorClassNameCharacters["*"] = "Multiplication";
-operatorClassNameCharacters["/"] = "Division";
-operatorClassNameCharacters["+"] = "Addition";
-operatorClassNameCharacters["-"] = "Subtraction";
-operatorClassNameCharacters["="] = "Equals";
-operatorClassNameCharacters["parentheses"] = "Parentheses";
+var operatorClassNameCharacters =
+{
+    "P": "P",
+    "sum": "Sum",
+    "cos": "Cos",
+    "arccos": "Arccos",
+    "!": "Factorial",
+    "^": "Power",
+    "rt": "Root",
+    "*": "Multiplication",
+    "/": "Division",
+    "+": "Addition",
+    "-": "Subtraction",
+    "=": "Equals",
+    "parentheses": "Parentheses"
+};

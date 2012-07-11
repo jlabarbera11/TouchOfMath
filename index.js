78227409d3a9ea29eb4b1a0be0ac0c1833312ae2
Zachary <zsclapper7@gmail.com>
2012-07-11 02:32:12 -0400

    Displaying a new expression now removes the current expression's history before displaying. Added undo keyboard shortcut ('Ctrl+Z')

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ var xmlstring = null;
 
 $(document).ready(function()
 {
+    $(document).keydown(
+        function(event)
+        {
+            // Only capture keyboard shortcuts if we are not in a text field
+            if(!(event.target.tagName.toLowerCase() == "textarea" || (event.target.tagName.toLowerCase() == "input" && $(event.target).attr("type") == "text")))
+            {
+                if(event.ctrlKey)
+                {
+                    //console.log(event.which);
+                    
+                    switch(event.which)
+                    {
+                        case 90:
+                            event.preventDefault();
+                            undo();
+                            break;
+                    }
+                }
+            }
+        }
+    );
+    
     setTimeout("$(window)[0].scrollTo(0, 0);", 500);
     
     $("body").bind("orientationchange",
@@ -62,12 +84,28 @@ $(document).ready(function()
             
             errorOccurred = false;
             
+            // Make sure a tree or math error has not occurred
             if(tree != null)
             {
-                displayEquation(xmlstring);
-                displayHistory(prevtrees);
-                postProcessing();
-                finalize();
+                // Check if the current expression's history (if it exists) needs to be removed before generating the new expression
+                if(
+                    prevtrees.length == 0 ||
+                    (
+                        prevtrees.length > 0 &&
+                        confirm(
+                            "This will start a new expression, " +
+                            "the current expression and its history will be removed."
+                        )
+                    )
+                )
+                {
+                    prevtrees = [];
+                    
+                    displayEquation(xmlstring);
+                    displayHistory(prevtrees);
+                    postProcessing();
+                    finalize();
+                }
             }
             else
             {
